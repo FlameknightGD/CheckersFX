9821d7f9107ab539fc318cc4c7f0545023547876
FlameknightGD <61699155+FlameknightGD@users.noreply.github.com>
2021-04-19 15:44:46 +0200

    File Changes 2021 04 19 | 15:44
    Changes made by Flame

--- src/game/Main.java
+++ src/game/Main.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -23,7 +24,13 @@ public class Main extends Application
 	double width;
 	double height;
 	
+	BorderPane pain = new BorderPane();
+	VBox menu_buttons = new VBox();
+	VBox back_button = new VBox();
+	
 	AudioClip its_raining_somewhere_else;
+	
+	int menu_id;
 	
 	public static void main(String[] args) 
 	{
@@ -51,7 +58,7 @@ public class Main extends Application
 		primaryStage.setMinHeight(720);
 		primaryStage.setMaxHeight(1080);
 		
-		//Dynamic Window Size
+		//Listenerst
 		primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> 
 		{
             width = (double) newVal;
@@ -78,6 +85,12 @@ public class Main extends Application
         root.setId("root");
         root.getStylesheets().add(getClass().getResource("game.css").toExternalForm());
         
+        root.getChildren().add(pain);
+		pain.setCenter(menu_buttons);
+		BorderPane.setAlignment(menu_buttons, Pos.CENTER);
+		
+		menu_buttons.setPrefSize(width, height);
+        
         //Scene Creation
         Scene scene = new Scene(root, 1280, 720);
         
@@ -94,36 +107,48 @@ public class Main extends Application
 	//Dynamic Window Methods
 	public void updateWidth(double newValue, double oldValue)
 	{
-		double multiplier = newValue/oldValue;
-        root.setPrefWidth(oldValue * multiplier);;
+		double multiplier = newValue / oldValue;
+        root.setPrefWidth(oldValue * multiplier);
+        //pain.setPrefWidth(oldValue * multiplier);
+        menu_buttons.setPrefWidth(oldValue * multiplier);
 	}
 	
 	public void updateHeight(double newValue, double oldValue)
 	{
 		double multiplier = newValue / oldValue;
         root.setPrefHeight(oldValue * multiplier);
+        //pain.setPrefHeight(oldValue * multiplier);
+        menu_buttons.setPrefHeight(oldValue * multiplier);
 	}
 	
 	//Screen Update
 	public void updateScreen()
 	{
-		root.getChildren().remove(0);
-		main_menu();
+		menu_buttons.getChildren().remove(0, menu_buttons.getChildren().size());
+		
+		if(menu_id == 1)
+		{
+			main_menu();
+		}
+		else if(menu_id == 2)
+		{
+			menu_singleplayer();
+		}
+		else if(menu_id == 3)
+		{
+			menu_multiplayer();
+		}
+		else if(menu_id == 4)
+		{
+			menu_settings();
+		}
 	}
 	
 	//Main Menu
 	public void main_menu()
 	{
-		//Pane Creation
-		BorderPane pain = new BorderPane();
-		
-		VBox sans = new VBox();
-		
-		root.getChildren().add(pain);
-		pain.setCenter(sans);
-		BorderPane.setAlignment(sans, Pos.CENTER);
-		
-		sans.setPrefSize(width, height);
+		//Menu ID change
+		menu_id = 1;
 		
 		//Buttons
 		Button button_sp = new Button("Singleplayer");
@@ -142,8 +167,8 @@ public class Main extends Application
 		button_settings.setMaxWidth(width / 4);
 		button_settings.setAlignment(Pos.CENTER);
 		
-		sans.getChildren().addAll(button_sp, button_mp, button_settings);
-		sans.setAlignment(Pos.CENTER);
+		menu_buttons.getChildren().addAll(button_sp, button_mp, button_settings);
+		menu_buttons.setAlignment(Pos.CENTER);
 		
 		//Button Functions
 		button_sp.setOnAction(new EventHandler<ActionEvent>() 
@@ -178,16 +203,54 @@ public class Main extends Application
 	
 	public void menu_singleplayer()
 	{
+		//Menu ID change
+		this.menu_id = 2;
 		
+		if(menu_buttons.getChildren().size() != 0)
+		{
+			menu_buttons.getChildren().remove(0, menu_buttons.getChildren().size());
+		}
+		
+		//Buttons
+		Button button_white = new Button("White");
+		Button button_black = new Button("Black");
+		Button button_back = new Button("Back");
+		
+		button_white.setId("menu_button");
+		button_white.setMaxWidth(width / 4);
+		button_white.setAlignment(Pos.CENTER);
+		
+		button_black.setId("menu_button");
+		button_black.setMaxWidth(width / 4);
+		button_black.setAlignment(Pos.CENTER);
+		
+		button_back.setId("menu_button");
+		button_back.setMaxWidth(width / 4);
+		button_back.setAlignment(Pos.CENTER);
+		
+		menu_buttons.getChildren().addAll(button_white, button_black);
+		menu_buttons.setAlignment(Pos.CENTER);
+		
+		back_button.getChildren().addAll(button_back);
+		back_button.setAlignment(Pos.TOP_LEFT);
 	}
 	
 	public void menu_multiplayer()
 	{
-
+		this.menu_id = 3;
 	}
 	
 	public void menu_settings()
 	{
-		
+		this.menu_id = 4;
+	}
+	
+	public void back()
+	{
+		if(menu_id >= 2 && menu_id <= 4)
+		{
+			main_menu();
+		}
+
 	}
 }
